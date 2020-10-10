0f818c3b8b45985f4924b714bf3fe9b4b48ace41
Antal János Monori <anthonymonori@gmail.com>
2020-10-10 14:07:34 +0100

    Added support for cors

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import typeDefs from './typedefs';
 import resolvers from './resolvers';
+import cors from 'cors';
 
 admin.initializeApp();
 
 const app = express();
+app.use(cors());
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: "/", cors: true });
 exports.graphql = functions.https.onRequest(app);
